72eb2911317f80df6ce6b41f96e4c4e6f94090f6
Bezdar <100799406+SolopovMisha@users.noreply.github.com>
2024-11-27 18:35:36 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,30 +3,28 @@ import dotenv
 import os
 import argparse
 from urllib.parse import urlparse
-from dotenv import load_dotenv
 
-load_dotenv()
 
 def shorten_link(telegram_token, long_url):
     url = 'https://api.vk.ru/method/utils.getShortLink'
-    
+
     params = {
-        "v": 5.199, 
+        "v": 5.199,
         "url": long_url
     }
     headers = {
         "Authorization": f"Bearer {telegram_token}"
     }
-    
+
     response = requests.post(url, headers=headers, params=params)
     response.raise_for_status()
-    
+
     return response.json()["response"]["short_url"]
 
 
 def count_clicks(telegram_token, short_link):
     url = 'https://api.vk.ru/method/utils.getLinkStats'
-    
+
     params = {
         "v": 5.236,
         "interval": "forever",
@@ -34,41 +32,40 @@ def count_clicks(telegram_token, short_link):
         "extended": 0
     }
     headers = {"Authorization": f"Bearer {telegram_token}"}
-    
+
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
-    
+
     data = response.json()
     if "response" in data and "stats" in data["response"] and len(data["response"]["stats"]) > 0:
         return data["response"]["stats"][0]['views']
     else:
-        return 0 # Или другое значение по умолчанию
+        return 0
+
 
 def main():
-    if __name__ == "__main__":
-        # Перенесите загрузку переменных окружения сюда
-        dotenv.load_dotenv()
-        vk_token = os.environ['VK_TOKEN']
+    dotenv.load_dotenv()
+    vk_token = os.environ['VK_TOKEN']
 
-        parser = argparse.ArgumentParser(
-            description='Сокращение ссылок'
-        )
-        parser.add_argument('user_url', help='Введите ссылку: ')
-        args = parser.parse_args()
-        # user_url = input("Введите ссылку: ")
-        parsed_url = urlparse(args.user_url)
+    parser = argparse.ArgumentParser(
+        description='Сокращение ссылок'
+    )
+    parser.add_argument('user_url', help='Введите ссылку: ')
+    args = parser.parse_args()
+    parsed_url = urlparse(args.user_url)
 
-        try:
-            if parsed_url.netloc == "vk.cc":
-                try:
-                    clicks = count_clicks(vk_token, parsed_url.path[1:])
-                    print("Количество кликов по ссылке:", clicks)
-                except requests.exceptions.HTTPError as e:
-                    print(f"HTTP error при подсчете кликов: {e}")
-            else:
-                print("Сокращённая ссылка:", shorten_link(vk_token, args.user_url))
-        except requests.exceptions.HTTPError as e:
-            print(f"HTTP error: {e}")
+    try:
+        if parsed_url.netloc == "vk.cc":
+            try:
+                clicks = count_clicks(vk_token, parsed_url.path[1:])
+                print("Количество кликов по ссылке:", clicks)
+            except requests.exceptions.HTTPError as e:
+                print(f"HTTP error при подсчете кликов: {e}")
+        else:
+            print("Сокращённая ссылка:", shorten_link(vk_token, args.user_url))
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error: {e}")
+
 
 if __name__ == "__main__":
     main()
